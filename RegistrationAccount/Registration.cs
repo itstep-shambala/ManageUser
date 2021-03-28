77ca65af4bee601fdba1cd839b5f5e2414ea35e2
Part_lo15 <jump-rostov@mail.ru>
2021-03-28 14:32:22 +0300

    исправление

--- RegistrationAccount/Registration.cs
+++ RegistrationAccount/Registration.cs
@@ -2,24 +2,18 @@
 using MySql.Data.MySqlClient;
 
 namespace RegistrationAccount
-{
-    
+{    
     public class Registration
     {
         private string host = "mysql60.hostland.ru";
         private string dateBase = "host1323541_itstep";
         private string userName = "host1323541_itstep35";
         private string password = "269f43dc";
-
-        public Registration(){ }
-
+        
         //кноструктор базы данных
         public Registration(string host, string dateBase, string userName, string password)
         {
-            this.host = host;
-            this.dateBase = dateBase;
-            this.userName = userName;
-            this.password = password;
+            SetDateBaseInfo(host, dateBase, userName, password);
         }
 
         //метод для передачи сигнатуры базы данных 
@@ -29,10 +23,7 @@ namespace RegistrationAccount
             this.dateBase = dateBase;
             this.userName = userName;
             this.password = password;
-        }
-
-        
-        
+        }        
         
         //показать всю таблицу
         public void SelectOllItemsInDate()
@@ -47,22 +38,19 @@ namespace RegistrationAccount
         }
         
         //запрос в базу данных
-        private void ExecuteARequest(string request)
+        private string ExecuteARequest(string request)
         {
             string connectionString = "Server=" + host + ";" + "Database=" + dateBase + ";" + "Uid=" + userName + ";" + "Pwd=" + password + ";charset=utf8;";
-
            
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {                
                 connection.Open();               
-                if (!connection.Ping()) { Console.WriteLine("ошибка"); return; }
+                if (!connection.Ping()) { Console.WriteLine("ошибка"); return null; }
               
                 var result = new MySqlCommand { Connection = connection, CommandText = request }.ExecuteReader();
-                
-               
+
+                return request;
             }
-        }
-        
-        
+        }        
     }
 }
